57b126434a112534398a946bdf62c72711ef219d
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-07-02 21:52:01 +0530

    Added Problem 3 final

--- problems/3-longest-substring-without-repeating-characters.c
+++ problems/3-longest-substring-without-repeating-characters.c
@@ -1,9 +1,6 @@
-#include <stdio.h>
-#include <string.h>
-
 struct Count {
-    char alphabet[63];
-    int count[63];
+    char alphabet[128];
+    int count[128];
 };
 
 int lengthOfLongestSubstring(char* s) {
@@ -11,20 +8,15 @@ int lengthOfLongestSubstring(char* s) {
     int maxval = 0;
 
     for (int i = 0; s[i] != '\0'; i++) {
-        for (int k = 0; k < 62; k++) {
-            c.alphabet[k] = (char)(65 + k);  
+        for (int k = 0; k < 128; k++) {
+            c.alphabet[k] = (char)(k);  
             c.count[k] = 0;
         }
-        c.alphabet[61] = ' ';   //testcase for ' '
-        c.count[61] = 0;
-        c.alphabet[62]='!';     //testcase for !
-        c.count[62]=0;
-
         int cn = 0; 
         for (int j = i; s[j] != '\0'; j++) {
             int repeated=0;
-            for (int k = 0; k < 62; k++) {
-                if (s[j] == c.alphabet[k] || s[j] == (c.alphabet[k]) - 32)
+            for (int k = 0; k < 127; k++) {
+                if (s[j] == c.alphabet[k])
                 {
                     if (c.count[k] == 0) 
                     {
