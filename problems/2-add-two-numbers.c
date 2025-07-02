cbfc37bc65cee86bb9159141fc0f44b66bde2cfa
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-07-02 13:07:06 +0530

    Added 02

--- problems/2-add-two-numbers.c
+++ problems/2-add-two-numbers.c
@@ -6,46 +6,44 @@
  * };
 */
 
-//32 test cases passed
 struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     struct ListNode* h1=l1;
     struct ListNode* h2=l2;
-    int n1 = 0, n2 = 0;
-    int placeval=1;
-    while(h1!=NULL || h2!=NULL)  // Changed from l1/l2 to h1/h2
+    struct ListNode* l3 = NULL;
+    struct ListNode* h3 = NULL;
+    int carry=0;
+    while(h1!=NULL || h2!=NULL || carry)  //testcase where carry remains
     {
+        int n1 = 0, n2 = 0, n3;
+        
         if (h1!=NULL)
         {
-            n1+=(h1->val)*placeval;
+            n1+=(h1->val);
             h1=h1->next;
         }
         if(h2!=NULL)
         {
-            n2+=(h2->val)*placeval;
+            n2+=(h2->val);
             h2=h2->next;
         }
-        placeval*=10;
-    }
-    int n3 = n2+n1;
-    int temp = n3;
-    struct ListNode* l3 = malloc(sizeof(struct ListNode));      //Always initialise, don't leave dangling pointers
-    l3->val=0;
-    l3->next=NULL;
-    struct ListNode* h3=l3;
-    while(temp!=0)
-    {
-        int digit = temp%10;
-        h3->val=digit;
-        temp/=10;
-        if(temp!=0) 
+        n3 = n2+n1+carry;
+        carry= n3/10;
+
+        struct ListNode * temp = malloc(sizeof(struct ListNode));
+        temp->val=n3%10;
+        temp->next=NULL;
+
+        if (l3==NULL)
         {
-            h3->next=malloc(sizeof(struct ListNode));
-            h3=h3->next;
-        } 
-        else 
-        {
-            h3->next = NULL;
+            l3=temp;
+            h3=temp;
         }
+        else
+        {
+            h3->next=temp;
+            h3=h3->next;
+        }
+        
     }
-    return l3;
+return l3;
 }
