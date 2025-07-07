07bb92622c5eecbd203e7c3c8426f971dd3b19de
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-07-07 22:59:56 +0530

    Added Problem 6

--- problems/6-zigzag-conversion.c
+++ problems/6-zigzag-conversion.c
@@ -1,27 +1,32 @@
 char* convert(char* s, int numRows) {
+    
     int len = strlen(s);
-    char* new = malloc((len + 1) * sizeof(char));
-    new[len] = '\0';
-    int current = 0;
-    int diff = 2 * numRows - 2;     //cycle length logic
+    char* new = malloc((len+1)*sizeof(char));
+    int current=0;
+    int diff= 2 * numRows - 2;
 
-    for (int i = 0; i<numRows && current<len; i++) 
+    if (numRows==1)
     {
-        int j = i;
-        while (j < len) 
+        new=s;
+        return new;
+    }
+
+    for (int i=0; i<numRows; i++) 
+    {
+        for (int j=i; j<len && current<len; j+=diff)      //current<len must for address error free
         {
-            new[current++] = s[j];
-            if (i != 0 && i != numRows - 1) 
+            new[current] = s[j];
+            current++;
+            int middle = j+diff-2*i;        //taken help with logic, need to revisit
+            if (i!=0 && i!=numRows-1 && middle<len) 
             {
-                int j2 = j+diff-2*i;
-                if (j2 < len) 
-                {
-                    new[current++] = s[j2];
-                }
+                new[current] = s[middle];
+                current++;
             }
-            j += diff;
         }
     }
 
+    new[current] = '\0'; 
     return new;
 }
+
