c033074a72842e1190911fc318d8e9d8cc1b6f23
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-07-08 12:04:45 +0530

    Added Problem 7

--- problems/7-reverse-integer.c
+++ problems/7-reverse-integer.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 int reverse(int x) {
 
     int temp = x;
@@ -6,6 +7,10 @@ int reverse(int x) {
     {
         int digit = temp%10;
         temp/=10;
+        if (reverse>INT_MAX/10 || reverse < INT_MIN/10)     //check for overflow, upper and lower bounds, reverse*10 doesn't work since can't be represented in int
+        {
+            return 0;
+        }
         reverse*=10;
         reverse+=digit;
     }
