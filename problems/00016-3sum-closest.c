b4a1e389556a0592585c2bdbe09d7f7be58b8c1e
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-07-19 23:36:21 +0530

    Added Problem 16 v4

--- problems/00016-3sum-closest.c
+++ problems/00016-3sum-closest.c
@@ -1,7 +1,13 @@
+//passed 73/106 testcases
 #include <stdio.h>
-
+#include <stdlib.h>
+struct dtos
+{
+    int diff;
+    int sum;
+};
 int threeSumClosest(int* nums, int numsSize, int target) {
-    int* diffarray = malloc(100*sizeof(int)); //since zero would represent the closest, can't initialise
+    struct dtos* arr1 = malloc(INT_MAX*sizeof(struct dtos)); //struct pointer to array of structures
     int c=0;    //current filled index
     for (int i=0; i<numsSize-2; i++)
     {
@@ -10,31 +16,22 @@ int threeSumClosest(int* nums, int numsSize, int target) {
             for (int k=j+1; k<numsSize; k++)
             {
                 int sum = nums[i]+nums[j]+nums[k];
-                int diff = sum-target;
-                if (sum>0)
-                {
-                    int diff = sum-target;
-                }
-                else
-                {
-                    int diff = (-sum)-target;
-                }
-                if (diff<0)
-                {
-                    diff = 0-diff;
-                }
-                printf("%d", diff);
-                diffarray[c++]=diff;
+                int diff = abs(sum - target);
+
+                arr1[c].diff=diff;
+                arr1[c++].sum=sum;
             }
         }
     }
-    int min = diffarray[0];
+    int min = arr1[0].diff;
+    int minindex=0;
     for (int x=1; x<c; x++)
     {
-        if (diffarray[x]<min)
+        if (arr1[x].diff<min)
         {
-            min=diffarray[x];
+            min=arr1[x].diff;
+            minindex = x;
         }
     }
-    return min;
+    return arr1[minindex].sum;
 }
