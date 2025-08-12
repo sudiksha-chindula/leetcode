eb3f0b5cd322b767c728e0bf8a5fc6540dd7ea25
Sudiksha Chindula <sudiksha.chindula7@gmail.com>
2025-08-12 21:56:32 +0530

    Added Problem 22 v1

--- problems/00021-merge-two-sorted-lists.c
+++ problems/00021-merge-two-sorted-lists.c
@@ -9,36 +9,54 @@
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2) {
     struct ListNode* l = list1;
     struct ListNode* r = list2;
-    struct ListNode* result = NULL;
-    struct ListNode* final = result;
-    while (l!=NULL && r!=NULL)
+    struct ListNode* result = NULL;   //tail
+    struct ListNode* final = NULL;    //head  
+
+    while (l != NULL && r != NULL)
     {
         struct ListNode* temp = NULL;
-        if (l->val<r->val)
+        if (l->val < r->val)
         {
             temp = malloc(sizeof(struct ListNode));
-            temp->val=l->val;
-            temp->next=NULL;
-            l=l->next;
+            temp->val = l->val;
+            temp->next = NULL;
+            l = l->next;
         }
         else
         {
             temp = malloc(sizeof(struct ListNode));
-            temp->val=r->val;
-            temp->next=NULL;
-            r=r->next;
+            temp->val = r->val;
+            temp->next = NULL;
+            r = r->next;
         }
-        result=temp;
-        result=result->next;
-        free(temp);
+
+        if (result == NULL)
+        {
+            result = temp;            
+            final = temp;             
+        }
+        else
+        {
+            result->next = temp;
+            result = result->next;   
+        }
     }
-    if (l!=NULL)
-    {
-        result=l;
+
+    if (final == NULL)  
+    {          
+        if (l!=NULL)
+        {
+            return l;
+        }
+        else
+        {
+            return r;
+        }
     }
-    if (r!=NULL)
-    {
-        result=r;
-    }
-    return final;
+    if (l != NULL)
+        result->next = l;
+    else if (r != NULL)
+        result->next = r;
+
+    return final;                    
 }
