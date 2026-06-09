d9b250a3aabd88bd45abce0acb36aa6d98cef9bd
sudiksha-chindula <sudiksha.chindula7@gmail.com>
2026-06-10 00:10:54 +0530

    Add Problem 217 with qsort()

--- problems/00217-contains-duplicate.c
+++ problems/00217-contains-duplicate.c
@@ -1,15 +1,34 @@
+//tried resolving TLE with bubble sort
+//bubble sort also takes O(n^2) (outer loop + pushing loop)
+//merge-sort and hash-sets too heavy for this code
+//require comparator function for qsort
+
+int compareInts(const void *a, const void *b)
+{
+    //dereference/cast pointers to integer values
+    int val1 = *(int*)a;
+    int val2 = *(int*)b;
+    //relation - val1 before val2, val2 after val1, equality
+    if (val1<val2) return -1;
+    if (val1>val2) return 1;
+    return 0;
+}
 bool containsDuplicate(int* nums, int numsSize) {
- for (int i=0; i<numsSize; i++)
- {
-    for (int j=i+1; j<numsSize; j++)
+ 
+    qsort(nums, numsSize, sizeof(int), compareInts); //4 arguments
+
+    for (int j=1; j<numsSize; j++)
     {
-        //printf("%d i\n", nums[i]);
-        //printf("%d j\n", nums[j]);
-        if (nums[i]==nums[j])
+        if (nums[j]==nums[j-1])
         {
+            //printf("%d, %d", nums[j-1], nums[j]);
+            while(nums[j]!=nums[j-1])
+            {
+                j++;
+            }
             return true;
         }
     }
+    return false;
+
  }   
- return false;
-}
